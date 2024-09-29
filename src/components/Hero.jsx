3c2f450b29b614d47ddf38790e9af962f606e85d
mariotbg11 <christofell.m@gmail.com>
2024-09-29 11:11:20 +0700

    feat: add & implement react router

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,40 @@
+import { Link } from "react-router-dom";
 import profile from "../assets/mario.png";
 
 function Hero() {
   return (
-    <div className="hero py-8">
-      <div className="hero-content text-center font-federoka">
-        <div className="max-w-lg">
-          <div className="avatar">
-            <div className="w-24 rounded-full">
-              <img src={profile} />
+    <div className="py-10 px-3">
+      <h1 className="text-3xl md:text-5xl lg:text-7xl font-semibold text-white">
+        Welcome
+      </h1>
+      <div className="divider"></div>
+      <div className="hero py-4">
+        <div className="hero-content text-center font-federoka">
+          <div className="max-w-lg">
+            <div className="avatar">
+              <div className="w-24 rounded-full">
+                <img src={profile} />
+              </div>
             </div>
+            <h1 className="text-lg font-medium text-white">
+              Hi there!, I'm Mario
+            </h1>
+            <p className="py-3 text-3xl lg:text-5xl font-medium text-white">
+              Lorem ipsum, dolor sit, amet, consectetur, adipisicing
+            </p>
+            <p className="py-3">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi. Lorem ipsum dolor sit amet consectetur adipisicing
+              elit.
+            </p>
+            <Link
+              to="/about"
+              className="btn bg-white text-black font-normal border-none rounded-full mt-4 px-8 uppercase tracking-wide hover:text-white"
+            >
+              About Me
+            </Link>
           </div>
-          <h1 className="text-lg font-medium text-white">Hi, I'm Mario</h1>
-          <p className="py-3 text-5xl font-medium text-white">
-            Lorem ipsum, dolor sit, amet, consectetur, adipisicing
-          </p>
-          <p className="py-3">
-            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-            excepturi exercitationem quasi. In deleniti eaque aut repudiandae et
-            a id nisi. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
-          <button className="btn bg-white text-black font-normal border-none rounded-full mt-4 px-8 uppercase tracking-wide hover:text-white">
-            About Me
-          </button>
         </div>
       </div>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,23 +1,57 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="navbar">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <Link to="/" className="btn btn-ghost text-2xl">
+          Mario
+        </Link>
       </div>
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Link</a>
+          <li className="hidden lg:block">
+            <Link to="/">Home</Link>
           </li>
-          <li>
+          <li className="hidden lg:block">
+            <Link to="/about">About</Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link to="/experience">Experience</Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link to="/project">Project</Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link to="/interest">Interest</Link>
+          </li>
+          <li className="hidden lg:block">
+            <Link to="/award">Award</Link>
+          </li>
+          <li className="block lg:hidden">
             <details>
-              <summary>Parent</summary>
-              <ul className="bg-base-100 rounded-t-none p-2">
+              <summary>Menu</summary>
+              <ul className="bg-base-100 rounded-t-none p-0">
                 <li>
-                  <a>Link 1</a>
+                  <Link to="/about">About</Link>
                 </li>
                 <li>
-                  <a>Link 2</a>
+                  <Link to="/experience">Experience</Link>
+                </li>
+                <li>
+                  <Link to="/skills">Skills</Link>
+                </li>
+                <li>
+                  <Link to="/project">Project</Link>
+                </li>
+                <li>
+                  <Link to="/interest">Interest</Link>
+                </li>
+                <li>
+                  <Link to="/award">Award</Link>
                 </li>
               </ul>
             </details>
